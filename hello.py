65482119b8f29c30c175cfc54bcf6e0de6e325bf
tsai00150 <tsai00150@outlook.com>
2022-11-05 19:57:23 -0400

    login template

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, redirect, render_template, request, session, url_for
 import os
 
 app = Flask(__name__)
@@ -11,16 +11,23 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return render_template('index.html')
+        return redirect(url_for('homepage'))
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
         session['logged_in'] = True
+        return redirect(url_for('homepage'))
     else:
-        flash('wrong password!')
+        return render_template('login.html')
 
-    return home()
+
+
+@app.route('/homepage', methods=['GET','POST'])
+def homepage():
+    print(request.form['tracksearch'])
+    return render_template('homepage.html')
+
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
