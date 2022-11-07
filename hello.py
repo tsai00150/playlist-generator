063b1054855748f49b11f9a2134e2c08c043f937
tsai00150 <tsai00150@outlook.com>
2022-11-06 22:37:49 -0500

    playlist & login

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask import Flask, redirect, render_template, request, session, url_for
 import os
 
@@ -17,16 +16,29 @@ def home():
 def do_admin_login():
     if request.form['password'] == 'p' and request.form['username'] == 'a':
         session['logged_in'] = True
+        session['username'] = 'a'
+        session['password'] = 'p'
         return redirect(url_for('homepage'))
     else:
         return render_template('login.html')
 
-
-
 @app.route('/homepage', methods=['GET','POST'])
 def homepage():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    if request.method =='POST':
+        if request.form.get('track_search_submit') == 'Search':
+            print(request.form['track_search'])
+        if request.form.get('filter_search_submit') == 'Create Playlist':
+            print(request.form)
+
     return render_template('homepage.html')
 
+@app.route('/playlists/', methods=['GET','POST'])
+def playlists():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    return render_template('playlists.html', username = session['username'])
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
