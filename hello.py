c1e387e2c6aedb088a8c6a5b77dd7c1fe92731d7
jinkezhao <jkzhao@ucdavis.edu>
2022-11-07 20:31:08 -0500

    commnet

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template, request, session, url_for
 import os
 from sqlalchemy import *
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 
@@ -9,6 +10,8 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 DATABASEURI = "postgresql://th2990:jz3581@w4111.cisxo09blonu.us-east-1.rds.amazonaws.com/proj1part2"
 engine = create_engine(DATABASEURI)
 conn = engine.connect()
+
+
 
 
 @app.route('/')
@@ -38,13 +41,19 @@ def homepage():
         if request.form.get('filter_search_submit') == 'Create Playlist':
             print(request.form)
 
+
     return render_template('homepage.html')
 
-@app.route('/playlists/', methods=['GET','POST'])
-def playlists():
+@app.route('/playlists', methods=['GET','POST'])
+def playlists(playlist_id):
     if not session.get('logged_in'):
         return render_template('login.html')
-    return render_template('playlists.html', username = session['username'])
+    query = text('''SELECT Track.track_name FROM TRACK WHERE Track.track_id in 
+(SELECT Track_has_playlist.track_id FROM Track_has_playlist WHERE Track_has_playlist.playlist_id = :id)''')
+    cursor = conn.execute(query,id = playlist_id)
+    sum = list(cursor.fetchall())
+    
+    return render_template('playlists.html', song = sum)
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
